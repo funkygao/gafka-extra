b66307235dea981b5c6588815c317d8eafcdb2fe
Funky Gao <funky.gao@gmail.com>
2017-02-03 16:51:25 +0800

    nil

---
+++ apps/raftd/log.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	glog "log"
+	"sync"
+
+	"github.com/hashicorp/raft"
+)
+
+var (
+	_ raft.LogStore    = &logStore{}
+	_ raft.StableStore = &logStore{}
+)
+
+// logStore implements raft.LogStore and raft.StableStore.
+type logStore struct {
+	sync.RWMutex
+
+	firstIndex, lastIndex uint64
+}
+
+func NewStore() *logStore {
+	return &logStore{}
+}
+
+// FirstIndex implements the LogStore interface.
+func (s *logStore) FirstIndex() (uint64, error) {
+	return s.firstIndex, nil
+}
+
+// LastIndex implements the LogStore interface.
+func (s *logStore) LastIndex() (uint64, error) {
+	return s.lastIndex, nil
+}
+
+// GetLog implements the LogStore interface.
+func (s *logStore) GetLog(index uint64, log *raft.Log) error {
+	glog.Printf("GetLog %d", index)
+	return nil
+}
+
+// StoreLog implements the LogStore interface.
+func (s *logStore) StoreLog(log *raft.Log) error {
+	return s.StoreLogs([]*raft.Log{log})
+}
+
+// StoreLogs implements the LogStore interface.
+func (s *logStore) StoreLogs(logs []*raft.Log) error {
+	glog.Printf("StoreLogs %+v", logs)
+
+	s.Lock()
+	defer s.Unlock()
+
+	for _, l := range logs {
+		if s.firstIndex == 0 {
+			s.firstIndex = l.Index
+		}
+		if l.Index > s.lastIndex {
+			s.lastIndex = l.Index
+		}
+	}
+
+	return nil
+}
+
+// DeleteRange implements the LogStore interface.
+func (s *logStore) DeleteRange(min, max uint64) error {
+	glog.Printf("DeleteRange %d=%d", min, max)
+
+	s.Lock()
+	defer s.Unlock()
+
+	for i := min; i <= max; i++ {
+
+	}
+
+	s.firstIndex = max + 1
+	return nil
+}
+
+// Set implements the StableStore interface.
+func (s *logStore) Get(key []byte) ([]byte, error) {
+	glog.Printf("Get %s", string(key))
+	return nil, nil
+}
+
+// GetUint64 implements the StableStore interface.
+func (s *logStore) GetUint64(key []byte) (uint64, error) {
+	glog.Printf("GetUint64 %s", string(key))
+	return 0, nil
+}
+
+func (s *logStore) Set(key, val []byte) error {
+	glog.Printf("Set %s:%s", string(key), string(val))
+	return nil
+}
+
+// SetUint64 implements the StableStore interface.
+func (s *logStore) SetUint64(key []byte, val uint64) error {
+	glog.Printf("SetUint64 %s:%d", string(key), val)
+	return nil
+}
